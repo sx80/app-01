d43e24bb62e06753caae4ccc1f90bffe24909da8
joeblack <joeblack@minint>
2017-07-28 17:09:17 +0200

    Initial

---
+++ app-01/src/main/java/org/sx/app/Main.java
@@ -0,0 +1,16 @@
+package org.sx.app;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+		
+		System.out.println("end....");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Main();
+	}
+
+}
